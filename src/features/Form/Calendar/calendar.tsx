9ec17dff43046b1f816289247acd67fb2b8cc3aa
Marcin Izdebski <mizdebski123@gmail.com>
2024-03-14 10:35:04 +0100

    Tanstack Query added

--- src/features/Form/Calendar/calendar.tsx
+++ src/features/Form/Calendar/calendar.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import { weekDay } from '../../../common/Arrays/arrays';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 
 export const Calendar = () => {
+
+    const country = 'PL';
+    const year = '2023';
+    const apiKey = '8DX8eEe67njS1lbThFsdSw==rQQNpQ8PYbPZBjrx';
+
+    const url = `https://api.api-ninjas.com/v1/holidays?country=${country}&year=${year}`;
+    const { isLoading, error, data } = useQuery({
+        queryKey: ['kaj'],
+        queryFn: () => axios
+            .get(url, {
+                headers: {
+                    'X-Api-Key': apiKey
+                },
+            })
+    });
     return (
         <div className="grid grid-cols-7 gap-2 mt-4">
             {weekDay.map((day) => (
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './core/App';
 import reportWebVitals from './core/reportWebVitals';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient();
+
 root.render(
-  <App />
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
