e959e5151f9605a89a5ae2022182e4fd7b94f9f8
Marcin Izdebski <mizdebski123@gmail.com>
2024-03-14 09:33:57 +0100

    Tailwind config added

--- src/features/Form/form.tsx
+++ src/features/Form/form.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Form = () => {
     return (
         <div className='max-w-[426px] m-auto'>
-            <h1>Personal Info</h1>
+            <h1 className='  text-2xl mb-6 text-[#000853] font-medium'>Personal Info</h1>
 
         </div>
     );
