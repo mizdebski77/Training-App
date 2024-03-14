ac7407680614706ad2becdade5b04d19f54821d9
Marcin Izdebski <mizdebski123@gmail.com>
2024-03-14 10:56:05 +0100

    Next, prev month buttons added

--- src/features/Form/Calendar/calendar.tsx
+++ src/features/Form/Calendar/calendar.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { weekDay } from '../../../common/Arrays/arrays';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
+import next from '../../../common/svg/next.svg';
+import prev from '../../../common/svg/prev.svg';
 
 export const Calendar = () => {
+
+    const currentDate = new Date();
+    const [currentYear, setYear] = useState(currentDate.getFullYear());
+    const [currentMonth, setMonth] = useState(currentDate.getMonth() + 1);
 
     const country = 'PL';
     const year = '2023';
@@ -22,7 +28,11 @@ export const Calendar = () => {
 
     const holidays = data?.data;
 
-    console.log(holidays);
+    const daysInMonth = (year: number, month: number) => new Date(year, month, 0).getDate();
+    const firstDayOfWeek = new Date(currentYear, currentMonth - 1, 1).getDay();
+
+
+
 
 
 
@@ -33,14 +43,25 @@ export const Calendar = () => {
             ) : error ? (
                 <span className='text-2xl'>Oops! Something went wrong...</span>
             ) : (
-                <div className="grid grid-cols-7 gap-2 mt-4">
-                    {weekDay.map((day) => (
-                        <div
-                            key={day}
-                            className="col-span-1 flex items-center justify-center font-medium text-sm">
-                            {day}
+                <div>
+                    <span>
+                        Date
+                    </span>
+                    <div className=" p-6 max-w-80 	bg-white border border-[#CBB6E5] rounded-lg">
+                        <div className="flex justify-between	 ">
+                            <button className="mr-2" >
+                                <img src={prev} alt='prev' />
+                            </button>
+
+                            <h1 className="text font-medium text-[#000853] ">
+                                {new Date(currentYear, currentMonth - 1, 1).toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}
+                            </h1>
+
+                            <button className="ml-2">
+                                <img src={next} alt='next' />
+                            </button>
                         </div>
-                    ))}
+                    </div>
                 </div>
             )}
         </div>
