d041d3f3a62f7173869ad7d3e067cf095e201e85
Marcin Izdebski <mizdebski123@gmail.com>
2024-03-14 10:46:01 +0100

    Loading and error handled

--- src/features/Form/Calendar/calendar.tsx
+++ src/features/Form/Calendar/calendar.tsx
@@ -11,7 +11,7 @@ export const Calendar = () => {
 
     const url = `https://api.api-ninjas.com/v1/holidays?country=${country}&year=${year}`;
     const { isLoading, error, data } = useQuery({
-        queryKey: ['kaj'],
+        queryKey: ['holidays'],
         queryFn: () => axios
             .get(url, {
                 headers: {
@@ -19,15 +19,32 @@ export const Calendar = () => {
                 },
             })
     });
-    return (
-        <div className="grid grid-cols-7 gap-2 mt-4">
-            {weekDay.map((day) => (
-                <div
-                    key={day}
-                    className="col-span-1 flex items-center justify-center font-medium text-sm">{day}</div>
-            ))}
 
+    const holidays = data?.data;
+
+    console.log(holidays);
+
+
+
+    return (
+        <div >
+            {isLoading ? (
+                <span className='text-2xl'>Downloading holiday data...</span>
+            ) : error ? (
+                <span className='text-2xl'>Oops! Something went wrong...</span>
+            ) : (
+                <div className="grid grid-cols-7 gap-2 mt-4">
+                    {weekDay.map((day) => (
+                        <div
+                            key={day}
+                            className="col-span-1 flex items-center justify-center font-medium text-sm">
+                            {day}
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
+
 };
 
